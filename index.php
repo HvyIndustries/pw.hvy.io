0803050f7d5c15bfe48cfd41e1baeb48bbe429bd
nevada_scout <joe.cotton@DEV007.ad.ijyi.com>
2015-11-11 14:10:50 +0000

    Changed tabs to spaces

--- index.php
+++ index.php
@@ -13,65 +13,65 @@ $pwSymbols = false;
 
 if(isset($_GET["l"]))
 {
-	$unsafe_pwLength = intval($_GET["l"]);
-	if($unsafe_pwLength != 0)
-	{
-		$pwCharLength = $unsafe_pwLength;
-		//var_dump($unsafe_pwLength);
-	}
+    $unsafe_pwLength = intval($_GET["l"]);
+    if($unsafe_pwLength != 0)
+    {
+        $pwCharLength = $unsafe_pwLength;
+        //var_dump($unsafe_pwLength);
+    }
 }
 
 function GenerateRandomChars($pwLength, $pwLower, $pwUpper, $pwNumbers, $pwSymbols) : string
 {
-	$char_lowercase = "abcdefghijkmnpqrstuvwxyz";
-	$char_uppercase = "ABCDEFGHJKLMNPQRSTUVWXYZ";
-	$char_numbers = "123456789";
-	$char_symbols = "#~@/$&*()^!+-%";
+    $char_lowercase = "abcdefghijkmnpqrstuvwxyz";
+    $char_uppercase = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+    $char_numbers = "123456789";
+    $char_symbols = "#~@/$&*()^!+-%";
 
-	// Build char list
-	$chars = "";
-	if($pwLower) { $chars .= $char_lowercase; }
-	if($pwUpper) { $chars .= $char_uppercase; }
-	if($pwNumbers) { $chars .= $char_numbers; }
-	if($pwSymbols) { $chars .= $char_symbols; }
+    // Build char list
+    $chars = "";
+    if($pwLower) { $chars .= $char_lowercase; }
+    if($pwUpper) { $chars .= $char_uppercase; }
+    if($pwNumbers) { $chars .= $char_numbers; }
+    if($pwSymbols) { $chars .= $char_symbols; }
 
-	$charLength = strlen($chars);
+    $charLength = strlen($chars);
 
-	$result = "";
+    $result = "";
 
-	for ($i = 0; $i < $pwLength; $i++)
-	{ 
-		$index = random_int(0, $charLength - 1);
-		$result .= substr($chars, $index, 1);
-	}
+    for ($i = 0; $i < $pwLength; $i++)
+    { 
+        $index = random_int(0, $charLength - 1);
+        $result .= substr($chars, $index, 1);
+    }
 
-	return $result;
+    return $result;
 }
 
 function GenerateRandomPhrase($phraseLength) : string
 {
-	$words = ["correct", "horse", "battery", "staple", "bridge", "code", "table", "tractor", "phone"];
+    $words = ["correct", "horse", "battery", "staple", "bridge", "code", "table", "tractor", "phone"];
 
-	$wordsLength = count($words);
+    $wordsLength = count($words);
 
-	$result = "";
+    $result = "";
 
-	for ($i = 0; $i < $phraseLength; $i++)
-	{ 
-		$index = random_int(0, $wordsLength - 1);
-		$result .= $words[$index];
-	}
+    for ($i = 0; $i < $phraseLength; $i++)
+    { 
+        $index = random_int(0, $wordsLength - 1);
+        $result .= $words[$index];
+    }
 
-	return $result;
+    return $result;
 }
 
 if($pwPhrase)
 {
-	$result = GenerateRandomPhrase($pwPhraseLength);
+    $result = GenerateRandomPhrase($pwPhraseLength);
 }
 else
 {
-	$result = str_shuffle(GenerateRandomChars($pwCharLength, $pwLower, $pwUpper, $pwNumbers, $pwSymbols));
+    $result = str_shuffle(GenerateRandomChars($pwCharLength, $pwLower, $pwUpper, $pwNumbers, $pwSymbols));
 }
 
 echo $result;
